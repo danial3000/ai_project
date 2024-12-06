12d1f182b21db97a0f802ce9f2674c89b4c48471
Mahdi <mmahdieh90@gmail.com>
2024-12-06 17:30:24 +0330

    deep

--- Phase2/Unknown Environment/dqn.py
+++ Phase2/Unknown Environment/dqn.py
@@ -4,6 +4,8 @@ import torch.optim as optim
 import numpy as np
 from qlearning import QLearning
 
+import pygame
+from environment import UnknownAngryBirds, PygameInit
 
 class DQNetwork(nn.Module):
     def __init__(self, input_dim, output_dim):
@@ -88,3 +90,52 @@ class DQLearning(QLearning):
                         max_indices[i, j, k] = np.random.choice(max_indices_at_point)
         self.q_policy = max_indices
         return self.q_policy
+
+
+if __name__ == "__main__":
+
+    env = UnknownAngryBirds()
+    screen, clock = PygameInit.initialization()
+    FPS = 10
+
+    ql = DQLearning(env=env, decay_rate=.995, learning_rate=0.9, discount_factor=0.8, epsilon_greedy=0.99)
+    values_difference, total_rewards = ql.explore(num_episodes=10000, conv_patience=10, conv_epsilon=10)
+    ql.plot_values_difference(values_difference, total_rewards)
+    policy = ql.set_policy()
+    ql.plot_policy(policy=policy)
+    state = env.reset()
+
+    episode_reward = []
+    for _ in range(5):
+
+        running = True
+        total_reward = 0
+        pig_state = [True for _ in range(8)]
+
+        while running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            env.render(screen)
+
+            action = policy[state[0], state[1], ql.get_config_index(pig_state)]
+            next_state, reward, pig_state, done = env.step(action)
+            state = next_state
+            total_reward += reward
+
+            if done:
+                print(pig_state)
+                print(f"Episode finished with reward: {total_reward}")
+                state = env.reset()
+                episode_reward.append(total_reward)
+                total_reward = 0
+                running = False
+
+            pygame.display.flip()
+            clock.tick(FPS)
+
+    print(f'MEAN REWARD: {sum(episode_reward)/len(episode_reward)}')
+
+    pygame.quit()
