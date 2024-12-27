1ca984f27ab14450a3e9d324dbbd53f61d1d2418
Mahdi <mmahdieh90@gmail.com>
2024-12-27 17:12:30 +0330

    implementation: * heuristic function effective factors: - total scores - safety distance for the hen - chase efficiency for the bird - proximity to endpoint - proximity to eggs - proximity to eggs - number of actions - number of eggs eaten by the hen - score of the bird eating the queen - early finish penalty - lose penalty
    * initializing the minimax class
    
    bug fix
    * minor update of the game.py files

--- Phase3/Optional/main.py
+++ Phase3/Optional/main.py
@@ -2,76 +2,6 @@ import numpy as np
 import pygame
 from game import AngryGame, PygameInit
 
-class MiniMax:
-    _goal = 1600
-    _actions_limit = 200
-
-    def __init__(self, environment):
-        self.env = environment
-        self.num_actions = environment.num_actions
-        self.endpoint = environment.get_slingshot_position(environment.grid)
-    
-    @staticmethod
-    def distance(pos1, pos2):
-        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
-    
-    def utility_function(self, grid, num_actions):
-        hen_position = self.env.get_hen_position(grid) # the escape agent
-        bird_position = self.env.get_bird_position(grid) # the chase agent
-        queen_position = self.env.get_queen_position(grid) # the hostile agent
-        egg_positions = self.env.get_egg_coordinate(grid) # list of (x, y) positions
-        pig_positions = self.env.get_pig_coordinate(grid) # list of (x, y) positions
-        endpoint = self.endpoint
-
-        hen_score = self._goal - 200 * len(self.env.get_egg_coordinate(self.env.grid))
-
-        finishing_factor = 0 if (hen_score - self._goal) / 1000 < 0 else (hen_score - self._goal) / 1000
-
-        # the bird score if reached appropriate total score
-        hen_score += 250 * finishing_factor if self.env.get_slingshot_position(grid) is None else 0
-
-        # the bird score if reached appropriate total score
-        bird_score = 600 * finishing_factor if self.env.is_queen_exists(grid) else 0
-
-        # the bird (chase agent) just finish the game, no scores
-        total_score = self.env.calculate_score(grid, num_actions)
-
-        # calculate the hen's safety (distance from hostile agent)
-        hen_safety = self.distance(hen_position, queen_position)
-
-        # calculate the bird's chase value (inverse distance to hostile agent)
-        bird_chase = 10 - self.distance(bird_position, queen_position)
-
-        # calculate proximity to endpoint for the hen
-        hen_to_endpoint = finishing_factor * (10 - self.distance(hen_position, endpoint))
-
-        # calculate proximity to eggs for the eggs
-        hen_to_eggs = sum(10 - self.distance(hen_position, egg_position) for egg_position in egg_positions)
-        # calculate proximity to pigs for the eggs
-        hen_to_pigs = sum(10 - self.distance(hen_position, pig_position) for pig_position in pig_positions)
-
-        # early finishing penalty
-        early_finish = self._goal - total_score if self.env.is_lose(grid, num_actions) or self.env.is_win(grid) else 0
-
-        # losing penalty
-        lose_penalty = 1000 if not self.env.is_hen_exists(grid) or num_actions == self._actions_limit else 0
-
-        # calculate final utility
-        utility = (
-                2 * total_score +  # prioritize collected points
-                3 * hen_safety +  # safety distance for the hen
-                3 * bird_chase +  # chase efficiency for the bird
-                2 * hen_to_endpoint + # proximity to endpoint
-                1 * hen_to_eggs - # proximity to eggs
-                1 * hen_to_pigs -  # proximity to eggs
-                1 * num_actions + # number of actions
-                1 * hen_score + # number of eggs eaten by the hen
-                1 * bird_score - # score of the bird eating the queen
-                1 * early_finish - # early finish penalty
-                1 * lose_penalty # lose penalty
-        )
-
-        return utility
 
 if __name__ == "__main__":
 
@@ -99,8 +29,6 @@ if __name__ == "__main__":
             if AngryGame.is_win(env.grid):
                 running = False
 
-
-
         if counter % 3 == 1:
             action = np.random.choice([0, 1, 2, 3])
             env.bird_step(action)
@@ -113,8 +41,6 @@ if __name__ == "__main__":
             env.render(screen)
             if AngryGame.is_lose(env.grid, env.num_actions):
                 running = False
-
-        print(env.grid)
 
         counter += 1
         pygame.display.flip()
